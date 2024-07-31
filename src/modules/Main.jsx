a28ff4d76542a76589d8a09bbf2cde72d74b57cd
Dima163163 <khlopovdima@gmail.com>
2024-07-31 16:28:34 +0400

    vite react media

---
+++ src/modules/Main.jsx
@@ -0,0 +1,12 @@
+import Products from './Products'
+import Promo from './Promo'
+
+const Main = () => {
+  return (
+    <main className="main">
+      <Promo/>
+      <Products/>
+    </main>
+  )
+}
+export default Main
